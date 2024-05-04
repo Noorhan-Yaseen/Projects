77fab11eccd39df3c96d81dadc157dcd909428cb
Noorhan Yaseen <115106853+Noorhan-Yaseen@users.noreply.github.com>
2024-05-04 16:56:50 +0500

    Add files via upload

---
+++ Landing website/script.js
@@ -0,0 +1,54 @@
+
+
+  const sr = ScrollReveal({
+      duration: 2000,
+      delay: 200,
+      reset: true
+  });
+
+  const animationConfig = {
+      distance: '50px',
+      origin: 'bottom',
+      opacity: 0,
+  };
+
+  const containers = document.querySelectorAll('.container-fluid, .container'); // Corrected selection
+  containers.forEach((container) => {
+      sr.reveal(container, animationConfig);
+  });
+
+  
+  let mybutton = document.getElementById("myBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
+
+
+    var coll = document.getElementsByClassName("collapsible");
+    var i;
+    
+    for (i = 0; i < coll.length; i++) {
+      coll[i].addEventListener("click", function() {
+        this.classList.toggle("active");
+        var content = this.nextElementSibling;
+        if (content.style.maxHeight){
+          content.style.maxHeight = null;
+        } else {
+          content.style.maxHeight = content.scrollHeight + "px";
+        } 
+      });
+    }
